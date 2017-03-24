443e941ef506ebea0e41be573a98118cc775c18c
ffxue <fanxue@outlook.com>
2017-03-24 18:49:39 +0800

    Test commit

---
+++ src/core/omg.h
@@ -0,0 +1,32 @@
+/**
+ * OMG, Optimization-based Model Generation
+ * Copyright (c) 2016 The University of Hong Kong
+ * Author: Fan Xue <xuef@hku.hk; fanxue@outlook.com>
+ *
+ * This file is part of omg.
+ *
+ * libcmaes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcmaes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcmaes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef OMG_H
+#define OMG_H
+namespace omg
+{
+    public class omg
+    {
+        
+    }
+}
+
+#endif
